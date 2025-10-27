f0f9af023f03439f63a1dfc8d7a298d0ea71432e
Hakan OBALI <hakanobali@hotmail.com>
2025-10-27 09:05:13 +0300

    eval düzenlemesi yapıldı

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,31 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `
+      default-src 'self';
+      script-src 'self' ${isDev ? "'unsafe-eval' 'unsafe-inline'" : ""} https: blob:;
+      connect-src 'self' https: wss: blob: data:;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: blob: https:;
+    `.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  };
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
